159411a62d898ae5819b834e1e6bbefab381f4cb
Christian Seidlitz <seidchr@gmail.com>
2013-08-15 01:32:21 +0200

    added the enumerable extension

---
+++ XEO.Core/Enumerable/EnumerableExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XEO.Core.Enumerable
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> list, T element)
+        {
+            foreach (var e in list)
+            {
+                yield return e;
+            }
+
+            yield return element;
+        }
+
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> list, IEnumerable<T> secondList)
+        {
+            foreach (var e in list)
+            {
+                yield return e;
+            }
+
+            foreach (var e in secondList)
+            {
+                yield return e;
+            }
+        }
+    }
+}
